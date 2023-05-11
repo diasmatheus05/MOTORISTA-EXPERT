6425f5fa9fca7c44846b1d7e4fc417256867211e
diasmatheus05 <diasmatheus05@gmail.com>
2023-05-11 20:57:47 -0300

    fix: day gradient

--- src/components/dashboard/dayGradient.tsx
+++ src/components/dashboard/dayGradient.tsx
@@ -1,44 +1,30 @@
 import { DayGradientColors } from "@/types";
 
 const colors_dic = {
-  0: "secondary-100",
-  20: "primary-100",
-  40: "primary-200",
-  60: "primary-300",
-  80: "primary-400",
-  100: "primary-500",
+  "00": "#a6a6a6", //"secondary-100",
+  "20": "#ffdb0d", //"primary-100",
+  "40": "#e8b10c", //"primary-200",
+  "60": "#ffa500", //"primary-300",
+  "80": "#e8810c", //"primary-400",
+  "100": "#ff6b0d", //"primary-500",
 };
 
 export function DayGradient({ colors }: { colors: DayGradientColors }) {
   return (
     <div className="w-full h-4 flex bg-secondary-100">
       <div
-        className={`w-full h-full bg-gradient-to-r from-${
-          colors_dic[colors["00"]]
-        } from-0% via-${colors_dic[colors["03"]]} via-50% to-${
-          colors_dic[colors["06"]]
-        } to-100%`}
-      />
-      <div
-        className={`w-full h-full bg-gradient-to-r from-${
-          colors_dic[colors["06"]]
-        } from-0% via-${colors_dic[colors["09"]]} via-50% to-${
-          colors_dic[colors["12"]]
-        } to-100%`}
-      />
-      <div
-        className={`w-full h-full bg-gradient-to-r from-${
-          colors_dic[colors["12"]]
-        } from-0% via-${colors_dic[colors["15"]]} via-50% to-${
-          colors_dic[colors["18"]]
-        } to-100%`}
-      />
-      <div
-        className={`w-full h-full bg-gradient-to-r from-${
-          colors_dic[colors["18"]]
-        } from-0% via-${colors_dic[colors["21"]]} via-50% to-${
-          colors_dic[colors["24"]]
-        } to-100%`}
+        className="w-full h-full"
+        style={{
+          background: `linear-gradient(90deg, ${colors_dic[colors["00"]]} 0%, ${
+            colors_dic[colors["03"]]
+          } 12.5%, ${colors_dic[colors["06"]]} 25%, ${
+            colors_dic[colors["09"]]
+          } 37.5%, ${colors_dic[colors["12"]]} 50%, ${
+            colors_dic[colors["15"]]
+          } 62.5%, ${colors_dic[colors["18"]]} 75%, ${
+            colors_dic[colors["21"]]
+          } 87.5%, ${colors_dic[colors["24"]]} 100%)`,
+        }}
       />
     </div>
   );
--- src/types/DayGradientColors.ts
+++ src/types/DayGradientColors.ts
@@ -1,4 +1,4 @@
-type Colors = 0 | 20 | 40 | 60 | 80 | 100;
+type Colors = "00" | "20" | "40" | "60" | "80" | "100";
 type Hours = "00" | "03" | "06" | "09" | "12" | "15" | "18" | "21" | "24";
 
 export type DayGradientColors = { [T in Hours]: Colors };
